3c3854398a01c8b164e1dae9542ec0eb6ab5733e
fikriadip <adifikri91@gmail.com>
2022-10-20 22:15:08 +0700

    initail commit

---
+++ dist/js/script.js
@@ -0,0 +1,59 @@
+// Navbar Fixed
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+// Hamburger Menu
+const hamburger = document.querySelector("#hamburger-menu");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Nav mobile click outside hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Add active description in portfolio projects
+$("#projects a").on("mouseover", function () {
+  $(this).find(".description").removeClass("opacity-0");
+  $(this).find(".description").addClass("description-active");
+});
+
+$("#projects a").on("mouseout", function () {
+  $(this).find(".description").addClass("opacity-0");
+  $(this).find(".description").removeClass("description-active");
+});
+
+// Dark mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  darkToggle.checked ? html.classList.add("dark") || (localStorage.theme = "dark") : html.classList.remove("dark") || (localStorage.theme = "light");
+});
+
+// Set the toggle position according to the mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#18d26e",
+        secondary: "#64748b",
+        tertiary: "#94a3b8",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
